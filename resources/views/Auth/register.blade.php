175093a5bdf871b3c1e3104257835f000b454fb8
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-01-18 22:52:39 -0300

    feat: cadastro do usuário

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -15,47 +15,53 @@
 <body class="w-screen">
     <main class="flex flex-1 min-h-screen">
         <section class="flex flex-1 bg-[url('/public/img/home-page/background2.png')] bg-cover bg-center bg-no-repeat">
-
         </section>
 
-        <section class="flex w-[40%]">
-            <form action="" method="" class="flex flex-col items-center gap-4 h-full w-full">
-                <div class="sticky w-full p-6">
-                    <a href="/">
-                        <img src="{{ asset('img/buttons/btn-return.svg') }}" class="cursor-pointer">
-                    </a>
+        <section class="flex flex-col w-[40%]">
+            <div class="w-full p-3">
+                <a href="/">
+                    <img src="{{ asset('img/buttons/btn-return.svg') }}" class="cursor-pointer">
+                </a>
+            </div>
 
-                    {{-- LOGO --}}
-                    <section class="flex flex-col items-center gap-3">
-                        <img src="{{ asset('img/logo.svg') }}" class="h-15 w-15">
-                        <span class="font-dosis text-primary md:text-[40px]">Cash Status</span>
+            <form action="{{ route('register') }}" method="POST"
+                class="flex flex-col justify-center items-center gap-4 h-full w-full">
+                @csrf
+
+                {{-- LOGO --}}
+                <section class="flex flex-col items-center">
+                    <img src="{{ asset('img/logo.svg') }}" class="h-15 w-15">
+                    <span class="font-dosis text-primary md:text-[40px]">Cash Status</span>
+
+                    <div>
                         <h1 class="font-dosis text-[30px]">Criar Conta</h1>
-                    </section>
-                </div>
-
+                     
+                    </div>
+                </section>
 
                 <div class="flex flex-col">
                     <label for="" class="text-xl">Nome</label>
-                    <input type="text" class="h-10 w-92 p-3 text-lg bg-tertiary rounded-xl shadow-sm">
+                    <input type="text" name="name" class="h-10 w-92 p-3 text-lg bg-tertiary rounded-xl shadow-sm">
                 </div>
 
                 <div class="flex flex-col">
                     <label for="" class="text-xl">Email</label>
-                    <input type="text" class="h-10 w-92 p-3 text-lg bg-tertiary rounded-xl shadow-sm">
+                    <input type="text" name="email" class="h-10 w-92 p-3 text-lg bg-tertiary rounded-xl shadow-sm">
                 </div>
 
                 <div class="flex flex-col">
                     <label for="" class="text-xl">Senha</label>
-                    <input type="password" class="h-10 w-92 p-3 text-lg bg-tertiary rounded-xl shadow-sm">
+                    <input type="password" name="password"
+                        class="h-10 w-92 p-3 text-lg bg-tertiary rounded-xl shadow-sm">
                 </div>
 
                 <a href="/login" class="text-md text-blue-500">Já tem uma conta?</a>
-                
+
                 <div class="flex gap-2 text-md">
                     <input type="checkbox" class="cursor-pointer">
                     <p>Mostrar Senha</p>
                 </div>
-                
+
                 <button
                     class="p-3 font-dosis text-secondary bg-primary rounded-md shadow-sm cursor-pointer">Registrar-Se</button>
             </form>
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+// 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/register', function () {
-    return view('Auth.register');
-});
-Route::get('/login', function () {
-    return view('Auth.login');
-});
+Route::get('/login', [AuthController::class, 'showFormLogin'])->name('show-form-login');
+
+Route::get('/register', [UserController::class, 'create'])->name('form-register');
+Route::post('/register', [UserController::class, 'store'])->name('register');
