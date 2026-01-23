f873e4f8ebb62a28f7ae53003f183847821cac2c
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-01-23 09:44:24 -0300

    feat: restrição de acesso do usuário a página de login e dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,24 @@ use App\Http\Controllers\UserController;
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-Route::get('/Profile', [ProfileController::class, 'index'])->name('profile');
-
-Route::get('/login', [AuthController::class, 'showFormLogin'])->name('form-login');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+});
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/register', [UserController::class, 'create'])->name('form-register');
-Route::post('/register', [UserController::class, 'store'])->name('register');
+Route::middleware([RedirectIfAuthenticated::class])->group(function () {
+    Route::get('/login', [AuthController::class, 'showFormLogin'])->name('form-login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+
+    Route::get('/register', [UserController::class, 'create'])->name('form-register');
+    Route::post('/register', [UserController::class, 'store'])->name('register');
+});
