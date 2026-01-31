7ce795cb52d111c9eb10a966c8ce4edcff62b344
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-01-31 15:09:49 -0300

    style: leveza no backdrop do menu

--- resources/views/Partials/menu.blade.php
+++ resources/views/Partials/menu.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed z-4 h-full w-full bg-black/60 backdrop-blur-sm" x-show="open" x-transition.opacity>
+<div class="fixed z-4 h-full w-full bg-black/60" x-show="open" x-transition.opacity>
     <aside class="h-full w-[30%] bg-secondary">
 
         <nav class="flex flex-col h-full w-full font-dosis text-xl">
@@ -18,7 +18,7 @@
                 </li>
 
                 <li>
-                    <a href="#" class="flex items-center w-full p-6 hover:bg-tertiary">
+                    <a href="" class="flex items-center w-full p-6 hover:bg-tertiary">
                         <img src="{{ asset('img/buttons/btn-account.svg') }}" class="h-10 w-10">
                         <span class="m-auto">Contas</span>
                     </a>
